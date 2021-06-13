30b168e5e09ec840dac23996c1c5068c061c50d4
AhmadSailik <asailik1993@gmail.com>
2021-06-13 23:00:13 +0300

    add location search bar and show map

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component{
+
+constructor(props){
+  super(props)
+  this.state={
+    locationResult:'',
+    errloc:'',
+    Map :false
+    
+  }
 }
 
+
+
+  generatLocation=async (event)=>{
+    event.preventDefault();
+    let location= event.target.addlocation.value;
+    // console.log(location);
+    let sharchLoc=`https://us1.locationiq.com/v1/search.php?key=pk.a7e855d378594e54a33745813035d5e3&q=${location}&format=json`
+
+    try{
+    let result =await axios.get(sharchLoc);
+    // console.log(result.data[0]);
+    
+    this.setState({
+      locationResult:result.data[0],
+      Map :true
+    })
+   }
+   catch{
+    this.setState({
+      errloc:'Try Again',
+      
+    })
+   }
+  }
+
+  render(){
+    return(
+      <div>
+        <h1>react axios</h1>
+        <form onSubmit={this.generatLocation}>
+          <input type='text'  placeholder="location" name='addlocation'/>
+          <button >submitit</button>
+        </form>
+        <p>{this.state.locationResult.display_name}</p>
+        <p>{this.state.locationResult.lon}</p>
+        <p>{this.state.locationResult.lat}</p>
+        <p>{this.state.errloc}</p>
+        {this.state.Map &&<img src={`https://maps.locationiq.com/v3/staticmap?key=pk.43fed3791d35ddb76aa14f749c6d3080&center=${this.state.locationResult.lat},${this.state.locationResult.lon}&size=400x400`} alt='map'/>}
+
+        
+      </div>
+    )
+  }
+}
 export default App;
