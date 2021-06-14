3691da87403b31c0f7d3e975ebd150020c371f6c
AhmadSailik <asailik1993@gmail.com>
2021-06-15 00:21:02 +0300

    add server to react

--- src/App.js
+++ src/App.js
@@ -10,7 +10,8 @@ constructor(props){
     locationResult:'',
     errloc:'',
     Map :false,
-   
+    weather:[],
+    listItemsWeath:''
   }
 }
 
@@ -20,28 +21,48 @@ constructor(props){
     event.preventDefault();
     let key=process.env.REACT_APP_APIofLocation;
     let location= event.target.addlocation.value;
-    // console.log(location);
-    let sharchLoc=`https://us1.locationiq.com/v1/search.php?key=${key}&q=${location}&format=json`
+   
+    let sharchLoc=`https://us1.locationiq.com/v1/search.php?key=${key}&q=${location}&format=json`;
+    let weatherLoc=`https://city-weathe.herokuapp.com/dataOfWeather?dataOfcity=${location}`;
 
     try{
     let result =await axios.get(sharchLoc);
-    // console.log(result.data[0]);
+    let resultWeater=await axios.get(weatherLoc);
     
     this.setState({
       locationResult:result.data[0],
-      Map :true
-    })
+      Map :true,
+      weather:resultWeater.data
+    });
+
+    if(location.toLowerCase() =='sydney'){
+      const listItems = this.state.weather.map((item) =>
+        <div>
+        <p >{item.description}</p>
+        <p >{item.date}</p>
+        </div>
+      );
+      this.setState({
+        listItemsWeath:listItems
+      });
+    }else{
+      this.setState({
+        listItemsWeath:resultWeater.data})
+    };
+
+    
    }
    catch{
     this.setState({
       errloc:'Try Again',
       
-    })
+    });
    }
-  }
+  };
 
   render(){
     let keys=process.env.REACT_APP_APIofLocation;
+  
     return(
     
       <div>
@@ -54,6 +75,7 @@ constructor(props){
         <p>{this.state.locationResult.display_name}</p>
         <p>{this.state.locationResult.lon}</p>
         <p>{this.state.locationResult.lat}</p>
+        {this.state.listItemsWeath }
         <p>{this.state.errloc}</p>
         {this.state.Map &&<img src={`https://maps.locationiq.com/v3/staticmap?key=${keys}&center=${this.state.locationResult.lat},${this.state.locationResult.lon}&size=400x400`} alt='map'/>}
 
